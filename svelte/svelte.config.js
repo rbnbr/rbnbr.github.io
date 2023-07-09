a7bbf47374203044858a814210295ed4fa5b4b76
Ruben Bauer <bauer.ruben98@googlemail.com>
2023-07-09 13:53:38 +0200

    missed '-t' flag for gh-pages (without it, it didn't copy the .nojekyll file)

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,15 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-    kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+ // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+ // for more information about preprocessors
+ preprocess: vitePreprocess(),
+// trailingSlash: 'always',
+
+ kit: {
+  adapter: adapter()
+ }
 };
+
+export default config;
